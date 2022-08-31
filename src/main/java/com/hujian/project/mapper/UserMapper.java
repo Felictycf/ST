54a3ed49c3b1eb89cacbfd2ad8c50b46a5d0a8ac
ravenxrz <zhang.xingrui@foxmail.com>
2022-08-31 21:10:38 +0800

    first commit

---
+++ src/main/java/com/hujian/project/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.hujian.project.mapper;
+
+import com.hujian.project.model.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * UserMapper
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
